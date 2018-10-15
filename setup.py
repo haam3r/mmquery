f67229a20541f11b9470d36848e563a29f00631e
Andres <andres@cert.ee>
2018-10-15 09:20:20 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mmquery',
-    version='0.1',
+    version='1.0',
     author="haam3r",
     description="CLI utility to query MatterMost API",
     long_description=long_description,
